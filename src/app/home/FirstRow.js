aaa2ef5e32151a11fab575d8e150f02918b30da1
Farhana <shaikfarhanacse@gmail.com>
2018-06-18 18:26:03 +0530

    commit

--- src/app/home/FirstRow.js
+++ src/app/home/FirstRow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Row,Col,Card,CardText} from "reactstrap";
+import {Row,Col,Card,CardText,CardTitle} from "reactstrap";
 import {Link} from "react-router-dom";
 import {imgStyle} from "../employee/LayoutSettings.css";
 import { users } from 'react-icons-kit/icomoon/users';
@@ -13,22 +13,22 @@ export class FirstRow extends React.Component{
       <Row className={styles.firstrowstyle}>
        <Col style={{marginRight:'-1vw'}}><Link to="/ViewEmployee" className={styles.linkStyle}>
         <Card body inverse className={styles.cardStyle} style={{ backgroundColor: '#A992E2'}}>
-        <CardText className={styles.cardTextStyle}>TOTAL EMPLOYEES<br/><b>14</b></CardText>
+        <p class="card-title" id={styles.cardTextStyle}>TOTAL EMPLOYEES<br/><b>14</b></p>
         </Card></Link>
         </Col>
        <Col style={{marginRight:'-1vw'}}><Link to="/NewJoiners" className={styles.linkStyle}>
            <Card body inverse className={styles.cardStyle} style={{ backgroundColor: '#EC6F59' }}>
-           <CardText className={styles.cardTextStyle}>NEW JOINERS<br/><b>6</b></CardText>
+           <p class="card-title" id={styles.cardTextStyle}>NEW JOINERS<br/><b>6</b></p>
          </Card></Link>
          </Col>
        <Col style={{marginRight:'-1vw'}}><Link to="/Leavers" className={styles.linkStyle}>
            <Card body inverse className={styles.cardStyle} style={{ backgroundColor: '#54BAE0'}}>
-           <CardText className={styles.cardTextStyle}>LEAVERS<br/> <b>1</b></CardText>
+           <p class="card-title" id={styles.cardTextStyle}>LEAVERS<br/> <b>1</b></p>
          </Card></Link>
          </Col>
        <Col style={{marginRight:'-1vw'}}><Link to="/AdminUsers" className={styles.linkStyle}>
          <Card body inverse className={styles.cardStyle} style={{ backgroundColor: '#F7C65F'}}>
-         <CardText className={styles.cardTextStyle}>ADMIN USERS<br/><b>2</b></CardText>
+         <p class="card-title" id={styles.cardTextStyle}>ADMIN USERS<br/><b>2</b></p>
        </Card></Link>
        </Col>
 
