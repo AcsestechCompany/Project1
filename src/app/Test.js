562a87cfe9a4a22b4018a3684b24597eb8af589a
Farhana <shaikfarhanacse@gmail.com>
2018-07-18 15:24:56 +0530

    commit

--- src/app/Test.js
+++ src/app/Test.js
@@ -4,30 +4,17 @@ import {Footer} from "./Footer";
 import styles from "./Layout.css";
 
 export class Test extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-
-    }
-  }
   handleClick(){
-
-  {
-    let me = 'go';  // globally scoped
-    var i = 'able';
-      console.log(me);
-        console.log(i + 'inside block');
+    window.location = "#/PersonalDetails";
   }
 
-  console.log(i + 'outside block inside function');
 
-  }
   render(){
     return(
       <div>
       <Header/>
       <div className={styles.displayContainer}>
-      <button class="btn btn-primary" onClick={this.handleClick.bind(this)}>Click ME !</button>
+    <button class="btn btn-primary" onClick={this.handleClick.bind(this)}>Go to Website </button>
       </div>
       <Footer/>
       </div>
